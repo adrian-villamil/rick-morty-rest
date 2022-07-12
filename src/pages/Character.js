3f0bdced2d01ef6509faf0ed304ba6bf64d0fdd8
adrian-villamil <adrian.villamil@unillanos.edu.co>
2022-07-11 19:46:20 -0500

    Added a table with the character information with some style.

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -7,14 +7,43 @@ const Character = () => {
 
   return (
     <div className="Character">
-      <img src={state.image} alt={state.name} />
-      <p>Name: {state.name}</p>
-      <p>Status: {state.status}</p>
-      <p>Species: {state.species}</p>
-      <p>Gender: {state.gender}</p>
-      <p>Origin: {state.origin.name}</p>
-      <p>Location: {state.location.name}</p>
-      <p>Created: {state.created}</p>
+      <div className="row image">
+        <img src={state.image} alt={state.name} />
+      </div>
+      <div className="row table">
+        <table>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>{state.name}</td>
+            </tr>
+            <tr>
+              <td>Status</td>
+              <td>{state.status}</td>
+            </tr>
+            <tr>
+              <td>Species</td>
+              <td>{state.species}</td>
+            </tr>
+            <tr>
+              <td>Gender</td>
+              <td>{state.gender}</td>
+            </tr>
+            <tr>
+              <td>Origin</td>
+              <td>{state.origin.name}</td>
+            </tr>
+            <tr>
+              <td>Location</td>
+              <td>{state.location.name}</td>
+            </tr>
+            <tr>
+              <td>Created</td>
+              <td>{state.created}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
