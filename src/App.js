f2b82ffa9eefedaf12818c93382c40beb41e1ed4
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-01-03 12:27:04 -0500

    Deleted the route to the Character component that I deleted too.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Character from './pages/Character';
-import Home from './pages/Home';
+import { HomePage } from './pages/HomePage';
 
-function App() {  
-
+function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path="/character" element={<Character />} />
+          <Route path='/' element={<HomePage />} />
         </Routes>
       </BrowserRouter>
     </div>
