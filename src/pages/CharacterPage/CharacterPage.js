c9442802b1858583f4a554bcafe6fa924466c8a3
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-01-03 18:39:13 -0500

    Created a new folder and component called CharacterPage where it will have the information of a single character.

---
+++ src/pages/CharacterPage/CharacterPage.js
@@ -0,0 +1,76 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import './CharacterPage.css';
+
+const URL = 'https://rickandmortyapi.com/api/character/';
+
+const CharacterPage = () => {
+  const [character, setCharacter] = useState({});
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const getCharacter = async () => {
+    try {
+      const response = await axios.get(URL.concat(id));
+      setCharacter(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCharacter();
+  }, []);
+
+  const returnToHomePage = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div className="CharacterPage">
+      <button onClick={returnToHomePage}>Volver atrás</button>
+      <h3>{character.name}</h3>
+      <img src={character.image} alt='character' />
+      <table>
+        <thead>
+          <tr>
+            <th colSpan={2}>Datos</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Status</td>
+            <td>{character.status}</td>
+          </tr>
+          <tr>
+            <td>Species</td>
+            <td>{character.species}</td>
+          </tr>
+          <tr>
+            <td>Type</td>
+            <td>{character.type}</td>
+          </tr>
+          <tr>
+            <td>Gender</td>
+            <td>{character.gender}</td>
+          </tr>
+          <tr>
+            <td>Origin</td>
+            <td>{character.origin?.name}</td>
+          </tr>
+          <tr>
+            <td>Location</td>
+            <td>{character.location?.name}</td>
+          </tr>
+          <tr>
+            <td>Created</td>
+            <td>{character.created}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export { CharacterPage };
