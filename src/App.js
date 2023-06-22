b44aff7e0704240cec7293705ff108d5aebc5377
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-06-22 01:32:00 -0500

    Added the AppLayout to all routes, and changed some styles to the global css.

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import { CharactersPage } from './pages/CharactersPage';
 import { LocationsPage } from './pages/LocationsPage';
 import { EpisodesPage } from './pages/EpisodesPage';
 import './App.css';
+import { AppLayout } from './layout/AppLayout';
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
-        <Header />
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/character' element={<CharactersPage />} />
-          <Route path='/location' element={<LocationsPage />} />
-          <Route path='/episode' element={<EpisodesPage />} />
-          <Route path='/character/:id' element={<SingleCharacterPage />} />
-          <Route path='*' element={<p style={{color: 'white'}}>404: Not found</p>} />
+          <Route path='/' element={<AppLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path='character' element={<CharactersPage />} />
+            <Route path='location' element={<LocationsPage />} />
+            <Route path='episode' element={<EpisodesPage />} />
+            <Route path='character/:id' element={<SingleCharacterPage />} />
+            <Route path='*' element={<p style={{color: 'white'}}>404: Not found</p>} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
