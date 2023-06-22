474637b3d18f4b52b291426311a1131f5d29cc87
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-06-22 09:14:11 -0500

    Added links to every character, location and episode page.

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,24 +2,31 @@ import CharactersImage from '../../assets/rick-and-morty-characters.png';
 import LocationsImage from '../../assets/rick-and-morty-locations.jpg';
 import EpisodesImage from '../../assets/rick-and-morty-episodes.jpg';
 import { DivContainer, DivItem, Image } from './styles';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <>
       <DivContainer>
         <DivItem>
+          <Link to='/character'>
+            <Image src={CharactersImage} alt="characters-image" />
+          </Link>
           <h1>Characters</h1>
-          <Image src={CharactersImage} alt="characters-image" />
           <p>Description</p>
         </DivItem>
         <DivItem>
+          <Link to='/location'>
+            <Image src={LocationsImage} alt="locations-image" />
+          </Link>
           <h1>Locations</h1>
-          <Image src={LocationsImage} alt="locations-image" />
           <p>Description</p>
         </DivItem>
         <DivItem>
+          <Link to='/episode'>
+            <Image src={EpisodesImage} alt="episodes-image" />
+          </Link>
           <h1>Episodes</h1>
-          <Image src={EpisodesImage} alt="episodes-image" />
           <p>Description</p>
         </DivItem>
       </DivContainer>
