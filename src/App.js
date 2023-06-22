be29fe41ee460047f18860b516b2d91bb0285805
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-06-21 20:57:51 -0500

    Added more routes for locations and episodes.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import { CharacterPage } from './pages/CharacterPage';
 import { HomePage } from './pages/HomePage';
 import { Header } from './components/Header';
+import { SingleCharacterPage } from './pages/SingleCharacterPage';
+import { CharactersPage } from './pages/CharactersPage';
+import { LocationsPage } from './pages/LocationsPage';
+import { EpisodesPage } from './pages/EpisodesPage';
+import './App.css';
 
 function App() {
   return (
@@ -11,7 +14,10 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<HomePage />} />
-          <Route path='/character/:id' element={<CharacterPage />} />
+          <Route path='/character' element={<CharactersPage />} />
+          <Route path='/location' element={<LocationsPage />} />
+          <Route path='/episode' element={<EpisodesPage />} />
+          <Route path='/character/:id' element={<SingleCharacterPage />} />
           <Route path='*' element={<p style={{color: 'white'}}>404: Not found</p>} />
         </Routes>
       </BrowserRouter>
