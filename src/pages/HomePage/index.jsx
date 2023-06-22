efad99b6e235daab6357176da16c21cdaa02dc07
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-06-22 01:28:12 -0500

    Added three divs for the characters, locations and episodes pages.

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,28 @@
+import CharactersImage from '../../assets/rick-and-morty-characters.png';
+import LocationsImage from '../../assets/rick-and-morty-locations.jpg';
+import EpisodesImage from '../../assets/rick-and-morty-episodes.jpg';
+import { DivContainer, DivItem, Image } from './styles';
+
 const HomePage = () => {
   return (
     <>
-      <h1>Home Page</h1>
+      <DivContainer>
+        <DivItem>
+          <h1>Characters</h1>
+          <Image src={CharactersImage} alt="characters-image" />
+          <p>Description</p>
+        </DivItem>
+        <DivItem>
+          <h1>Locations</h1>
+          <Image src={LocationsImage} alt="locations-image" />
+          <p>Description</p>
+        </DivItem>
+        <DivItem>
+          <h1>Episodes</h1>
+          <Image src={EpisodesImage} alt="episodes-image" />
+          <p>Description</p>
+        </DivItem>
+      </DivContainer>
     </>
   );
 };
---
+++ src/pages/HomePage/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const DivContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  width: 100%;
+`;
+
+export const DivItem = styled.div`
+  width: 400px;
+  margin: 10px;
+  border: 1px solid gray;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+`;
