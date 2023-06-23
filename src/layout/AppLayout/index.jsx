ef265029b4dc8feb6d82943adbbaa6bc07be0e66
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-06-23 11:22:36 -0500

    Fixed an issue with flexbox where the main content was not styling how I wanted to be.

--- src/layout/AppLayout/index.jsx
+++ src/layout/AppLayout/index.jsx
@@ -1,15 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
-import { Main } from "./styles";
 
 export const AppLayout = () => {
   return (
     <>
       <Header />
-      <Main>
-        <Outlet />
-      </Main>
+      <Outlet />
       <Footer />
     </>
   );
