e7c314540b07483e23392360bcb13cc3b59c8574
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-06-25 18:54:00 -0500

    Using the custom hook to fetch the data but just showing only names of every episode and location.

--- src/pages/LocationsPage/index.jsx
+++ src/pages/LocationsPage/index.jsx
@@ -1,9 +1,44 @@
+import { useState } from "react";
+import { useAxios } from "../../hooks/useAxios";
 import { LocationsDiv } from "./styles";
+import { Pagination } from "../../components/Pagination";
 
 export const LocationsPage = () => {
+  const [page, setPage] = useState(1);
+  const { isLoading, error, data } = useAxios(`location?page=${page}`);
+
+  if (isLoading) return <p>Is loading...</p>
+
+  if (error) return <p>There was an error</p>
+
+  const toNextPage = () => {
+    setPage(page => page + 1);
+  };
+
+  const toPreviousPage = () => {
+    setPage(page => page - 1);
+  };
+
   return (
     <LocationsDiv>
       <h1>Locations Page</h1>
+      <Pagination
+        nextPage={data?.info.next}
+        previousPage={data?.info.prev}
+        toNextPage={toNextPage}
+        toPreviousPage={toPreviousPage}
+      />
+      <ul>
+        {data?.results.map((location) => (
+          <li key={location.id}>{location.name}</li>
+        ))}
+      </ul>
+      <Pagination
+        nextPage={data?.info.next}
+        previousPage={data?.info.prev}
+        toNextPage={toNextPage}
+        toPreviousPage={toPreviousPage}
+      />
     </LocationsDiv>
   );
 };
