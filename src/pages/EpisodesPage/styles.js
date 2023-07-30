779792f5251a44057e51304c86603532ebe08314
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-07-29 21:17:45 -0500

    Deleted the pages for the locations and episodes.

--- src/pages/EpisodesPage/styles.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const EpisodesDiv = styled.div`
-  flex: 1;
-`;
--- src/pages/LocationsPage/styles.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const LocationsDiv = styled.div`
-  flex: 1;
-`;
