e99356bb790fa976efbf296d82548bdefe22579e
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-07-29 22:33:45 -0500

    Added a gap between every card item.

--- src/components/CardList/styles.js
+++ src/components/CardList/styles.js
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const CardListWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-around;
-  align-items: flex-start;
+  justify-content: center;
+  gap: 30px;
 `;
