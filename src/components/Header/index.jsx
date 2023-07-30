e73832b38a1dcabb4b70b2d398a34e7946eb879d
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-07-29 22:36:31 -0500

    Removed some navlinks

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,3 @@
-import RickIcon from '../../assets/icons8-rick-sanchez.svg';
-import MortyIcon from '../../assets/icons8-morty-smith.svg';
 import { HeaderWrapper, NavIcon, NavLink } from './styles';
 
 const Header = () => {
@@ -8,15 +6,6 @@ const Header = () => {
     <HeaderWrapper>
       <NavLink to='/'>
         Home
-      </NavLink>
-      <NavLink to='/character'>
-        Characters
-      </NavLink>
-      <NavLink to='/location'>
-        Locations
-      </NavLink>
-      <NavLink to='/episode'>
-        Episodes
       </NavLink>
     </HeaderWrapper>
   );
