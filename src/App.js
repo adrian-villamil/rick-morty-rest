c2ec7160ed5bbda3bc851e94f3ed938910bfb087
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-07-29 22:36:51 -0500

    Removed some paths.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
-import { Header } from './components/Header';
-import { SingleCharacterPage } from './pages/SingleCharacterPage';
-import { CharactersPage } from './pages/CharactersPage';
-import { LocationsPage } from './pages/LocationsPage';
-import { EpisodesPage } from './pages/EpisodesPage';
+import { CharacterPage } from './pages/CharacterPage';
 import './App.css';
 import { AppLayout } from './layout/AppLayout';
 
@@ -15,10 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<AppLayout />}>
             <Route index element={<HomePage />} />
-            <Route path='character' element={<CharactersPage />} />
-            <Route path='location' element={<LocationsPage />} />
-            <Route path='episode' element={<EpisodesPage />} />
-            <Route path='character/:id' element={<SingleCharacterPage />} />
+            <Route path='character/:id' element={<CharacterPage />} />
             <Route path='*' element={<p style={{color: 'white'}}>404: Not found</p>} />
           </Route>
         </Routes>
