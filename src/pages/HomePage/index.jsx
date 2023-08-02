e937b55ef72f182ff4f78168e12f695e0711a8b5
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-08-02 02:37:47 -0500

    Changed useState hook for useSearchParams so I can fetch data based on the page search param.

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,25 +1,24 @@
 import { CardList } from '../../components/CardList';
-import { useState } from "react";
 import { CardItem } from "../../components/CardItem";
 import { HomeWrapper } from "./styles";
 import { useAxios } from "../../hooks/useAxios";
-import { Paginate } from '../../components/Paginate';
+import { Pagination } from '../../components/Pagination';
+import { useSearchParams } from 'react-router-dom';
 
 export const HomePage = () => {
-  const [page, setPage] = useState(1);
-  const { loading, error, data } = useAxios(`character/?page=${page}`);
+  const [searchParams, setSearchParams] = useSearchParams({ page: '1' });
+  const { loading, error, data } = useAxios(`character/?page=${searchParams.get('page')}`);
 
   if (loading) return <HomeWrapper><p>Is loading...</p></HomeWrapper>;
 
   if (error) return <HomeWrapper><p>There was an error:</p></HomeWrapper>;
 
   const onPageChange = (pageNumber) => {
-    setPage(pageNumber);
+    setSearchParams({ page: pageNumber });
   };
 
   return (
     <HomeWrapper>
-      <Paginate pageCount={data?.info.pages} onPageChange={onPageChange} />
       <CardList>
         {data?.results.map(character => (
           <CardItem
@@ -28,7 +27,7 @@ export const HomePage = () => {
           />
         ))}
       </CardList>
-      {/* <Paginate pageCount={data?.info.pages} onPageChange={onPageChange} /> */}
+      <Pagination pageCount={data?.info.pages} onPageChange={onPageChange} />
     </HomeWrapper>
   );
 };
